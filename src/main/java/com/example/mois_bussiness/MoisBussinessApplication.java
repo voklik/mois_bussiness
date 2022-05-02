7077127e477c1bfd5ce21a2c64d017c0b5e4be7a
Michael Souček <michael.soucek1@seznam.cz>
2022-05-02 17:34:56 +0200

    code refactor

--- src/main/java/com/example/mois_bussiness/MoisBussinessApplication.java
+++ src/main/java/com/example/mois_bussiness/MoisBussinessApplication.java
@@ -2,8 +2,10 @@ package com.example.mois_bussiness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MoisBussinessApplication {
 
     public static void main(String[] args) {
